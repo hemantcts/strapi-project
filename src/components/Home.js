0ad5a42655d79353697028272759bce525f16052
kavitacts123 <kavitakavita.cts@gmail.com>
2025-01-24 19:04:23 +0530

    responsive desgin

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import Navbar from './Navbar';
 import heartImg from '../images/HEART.png';
-import productImg1 from '../images/product-img1.png'
+import productImg1 from '../images/product-img1.png';
+import adImg from '../images/ad-img.png';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
@@ -13,22 +15,22 @@ const Home = () => {
                 <div className="banner">
                     <div className="container">
                         <div className="row">
-                            <div className="col-lg-6 text-center">
+                            <div className="col-lg-6 text-center mb-5 mb-lg-0">
                                 <h1>Apotheke und Praxis unter einem Dach</h1>
                                 <p>Unser Team aus Hausärzt/innen, Gynäkolog/ irurg/innen und Apotheker/innen arbeitet interdisziplinär, um Sie individuell und kompetent zu betreuen.</p>
                                 <p>Wir freuen uns auf Ihren Besuch!</p>
                                 <ul className='p-0'>
                                     <li>
-                                        <h5 className='m-0 option-link'>ZUR APOTHEKE</h5>
+                                        <Link to='/' className='m-0 option-link'>ZUR APOTHEKE <i class="fa-solid fa-arrow-right fa-fade"/></Link>
                                     </li>
                                     <li>
-                                        <h5 className='m-0 option-link'>ZUR PRAXIS</h5>
+                                        <Link to='/' className='m-0 option-link'>ZUR PRAXIS <i class="fa-solid fa-arrow-right fa-fade"/></Link>
                                     </li>
                                 </ul>
                             </div>
                             <div className="col-lg-6">
-                                <div className="d-flex banner-item">
-                                    <div className="round round-1 me-4">TERMIN FÜR DIE APOTHEKE BUCHEN</div>
+                                <div className="d-flex banner-item justify-content-lg-end align-items-lg-end justify-content-around align-items-start">
+                                    <div className="round round-1">TERMIN FÜR DIE APOTHEKE BUCHEN</div>
                                     <div className="round round-2">TERMIN FÜR DIE PRAXIS BUCHEN</div>
                                 </div>
                             </div>             
@@ -38,34 +40,36 @@ const Home = () => {
             </section>
 
             <section className="heart-sec">
-                <div className="container ps-0">
+                <div className="container ps-lg-0">
                     <div className="row justify-content-between">
-                        <div className="col-lg-7 ps-0">
-                            <div className="row align-items-center">
-                                <div className="col-4 ps-0">
-                                    <img src={heartImg} alt="" />
-                                </div>
-                                <div className="col-8">
-                                    <div className="heart-inner-content1">
-                                        <h5>Gesundheitsvorsorge </h5>
-                                        <h2>HerzCheck</h2>
-                                        <p>
-                                            Neun von zehn Herzinfarkte und Hirnschläge
-                                            werden von Faktoren beeinflusst, die sich messen
-                                            und kontrollieren lassen. Mit dem HerzCheck
-                                            erfahren Sie, wie es um Ihre Herz-Kreislauf Gesundheit steht. Wenn Sie Ihre Risikofaktoren
-                                            kennen, können Sie aktiv etwas dagegen tun.
-                                        </p>
-                                        <ul className='p-0'>
-                                            <li>
-                                                <h5 className='m-0 option-link'>WEITERLESEN</h5>
-                                            </li>
-                                        </ul>
+                        <div className="col-lg-7 p-lg-0">
+                            <div className="heart-inner-content1 mb-5 mb-lg-0">
+                                <div className="row align-items-center mb-5">
+                                    <div className="col-4 ps-lg-0">
+                                        <img src={heartImg} alt="" />
+                                    </div>
+                                    <div className="col-8">
+                                        <div className="heart-sec-inner-content">
+                                            <h5>Gesundheitsvorsorge </h5>
+                                            <h2>HerzCheck</h2>
+                                            <p>
+                                                Neun von zehn Herzinfarkte und Hirnschläge
+                                                werden von Faktoren beeinflusst, die sich messen
+                                                und kontrollieren lassen. Mit dem HerzCheck
+                                                erfahren Sie, wie es um Ihre Herz-Kreislauf Gesundheit steht. Wenn Sie Ihre Risikofaktoren
+                                                kennen, können Sie aktiv etwas dagegen tun.
+                                            </p>
+                                            <ul className='p-0'>
+                                                <li>
+                                                    <Link to='/' className='m-0 option-link'>WEITERLESEN <i class="fa-solid fa-arrow-right fa-fade"/></Link>
+                                                </li>
+                                            </ul>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div className="col-lg-4">
+                        <div className="col-lg-4 plg--0">
                             <div className="heart-inner-content2">
                                 <div className="item-1 mb-4">
                                     <h5>Impfungen</h5>
@@ -78,7 +82,7 @@ const Home = () => {
                                     </p>
                                     <ul className='p-0'>
                                         <li>
-                                            <h5 className='m-0 option-link'>WEITERLESEN</h5>
+                                            <Link to='/' className='m-0 option-link'>WEITERLESEN <i class="fa-solid fa-arrow-right fa-fade"/></Link>
                                         </li>
                                     </ul>
                                 </div>
@@ -90,7 +94,7 @@ const Home = () => {
                                     chronische Wunden professionell versorgt.</p>
                                     <ul className='p-0'>
                                         <li>
-                                            <h5 className='m-0 option-link'>WEITERLESEN</h5>
+                                            <Link to='/' className='m-0 option-link'>WEITERLESEN <i class="fa-solid fa-arrow-right fa-fade"/></Link>
                                         </li>
                                     </ul>
                                 </div>
@@ -105,13 +109,13 @@ const Home = () => {
                 <div className="container">
                     <h2 className='text-center mb-4'>Aktionen</h2>
                     <div className="row">
-                        <div className="col-6">
-                            <div>
+                        <div className="col-lg-6">
+                            <div className="product-items mb-5 mb-lg-0">
                                 <div className="row">
                                     <div className="col-12">
                                         <div className="row">
                                             <div className="col-7">
-                                                <div className='product-item'>
+                                                <div className='product-item mb-4'>
                                                     <h4>Grippe</h4>
                                                     <p>Pretuval® eignet sich zur Behandlung einer
                                                         Erkältung oder einer Grippe und lindert
@@ -125,7 +129,7 @@ const Home = () => {
                                             </div>
                                         </div>
                                     </div>
-                                    <div className="col-10">
+                                    <div className="col-md-10 col-sm-8 ">
                                         <div className="product-info">
                                             <h5>Pretuval® Grippe & Erkältung</h5>
                                             <div className="d-flex align-items-baseline justify-content-between">
@@ -167,7 +171,7 @@ const Home = () => {
                                         <div className='about-product'>
                                             <ul className='p-0'>
                                                 <li>
-                                                    <h5 className='option-link'>BESTELLEN</h5>
+                                                    <Link to='/' className='option-link'>BESTELLEN <i class="fa-solid fa-arrow-right fa-fade"/></Link>
                                                 </li>
                                             </ul>
 
@@ -179,13 +183,13 @@ const Home = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className="col-6">
+                        <div className="col-lg-6">
                             <div>
                                 <div className="row">
                                     <div className="col-12">
                                         <div className="row">
                                             <div className="col-7">
-                                                <div className='product-item'>
+                                                <div className='product-item mb-4'>
                                                     <h4>Grippe</h4>
                                                     <p>Pretuval® eignet sich zur Behandlung einer
                                                         Erkältung oder einer Grippe und lindert
@@ -199,8 +203,8 @@ const Home = () => {
                                             </div>
                                         </div>
                                     </div>
-                                    <div className="col-10">
-                                        <div className="product-info">
+                                    <div className="col-md-10 col-sm-8">
+                                        <div className="product-info ">
                                             <h5>Pretuval® Grippe & Erkältung</h5>
                                             <div className="d-flex align-items-baseline justify-content-between">
                                                 <span>
@@ -241,7 +245,7 @@ const Home = () => {
                                         <div className='about-product'>
                                             <ul className='p-0'>
                                                 <li>
-                                                    <h5 className='option-link'>BESTELLEN</h5>
+                                                    <Link to='/' className='option-link'>BESTELLEN <i class="fa-solid fa-arrow-right fa-fade"/></Link>
                                                 </li>
                                             </ul>
 
@@ -260,8 +264,8 @@ const Home = () => {
             <section className='ad-sec'>
                 <div className="container">
                     <h2 className='text-center'> Unsere Partner </h2>
-                    <div className="ad-inner-content">
-
+                    <div className="ad-inner-content mt-5 mb-3">
+                        <img src={adImg} alt="" />
                     </div>
                 </div>
 
@@ -270,14 +274,14 @@ const Home = () => {
             <footer className='footer-sec'>
                 <div className="container">
                     <div className="row">
-                        <div className="col-3">
+                        <div className="col-lg-3 col-sm-6 mb-4 mb-lg-0">
                             <h5>MedZentrum Pfungen</h5>
                             <ul className='p-0'>
                                 <li>Riedäckerstrasse 5</li>
                                 <li>8422 Pfungen</li>
                             </ul>
                         </div>
-                        <div className="col-3">
+                        <div className="col-lg-3 col-sm-6 mb-4 mb-lg-0">
                             <h5>Apotheke</h5>
                             <ul className='p-0'>
                                 <li>052 305 03 55</li>
@@ -291,7 +295,7 @@ const Home = () => {
                             </ul>
                             
                         </div>
-                        <div className="col-3">
+                        <div className="col-lg-3 col-sm-6 mb-4 mb-lg-0">
                             <h5>Praxis</h5>
                             <ul className='p-0'>
                                 <li>052 305 03 55</li>
@@ -309,7 +313,7 @@ const Home = () => {
                                 <li>Samstag nach Vereinbarung</li>
                             </ul>
                         </div>
-                        <div className="col-3">
+                        <div className="col-lg-3 col-sm-6 mb-4 mb-lg-0">
                             <h5>Rechtliches</h5>
                             <ul className='p-0'>
                                 <li>Impressum</li>
